b54ffb753d58a3d7dbbb179e5d2c86d56992a673
shahirsizan <shahir.sizan18@gmail.com>
2024-05-30 22:12:06 +0600

    Added some dummy data

--- server.js
+++ server.js
@@ -4,12 +4,32 @@ const expressGraphQL = require("express-graphql").graphqlHTTP;
 // because `expressGraphQL` function is inside the object returned by `express-graphql`.
 // so we have to destructure it
 
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
+const authors = [
+	{ id: 1, name: "J. K. Rowling" },
+	{ id: 2, name: "J. R. R. Tolkien" },
+	{ id: 3, name: "Brent Weeks" },
+];
+
+const books = [
+	{ id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+	{ id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+	{ id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+	{ id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+	{ id: 5, name: "The Two Towers", authorId: 2 },
+	{ id: 6, name: "The Return of the King", authorId: 2 },
+	{ id: 7, name: "The Way of Shadows", authorId: 3 },
+	{ id: 8, name: "Beyond the Shadows", authorId: 3 },
+];
+
 const app = express();
 
 app.use(
 	"/graphql",
 	expressGraphQL({
 		graphiql: true,
+		schema: schema,
 	})
 );
 
